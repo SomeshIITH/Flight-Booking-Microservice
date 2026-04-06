73421ec1f26dbe453eea7124f19100392b04f883
SomeshIITH <shivjaiswal51230@gmail.com>
2026-04-06 18:29:48 +0530

    added message queue logic

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,62 +2,91 @@ const {StatusCodes} = require('http-status-codes');
 
 const {BookingService} = require('./../service/index.js');
 const bookingService = new BookingService();
+const {createChannel,publishMessage} = require('./../utils/message-queue.js')
+const {REMINDER_BINDING_KEY} = require('./../config/serverConfig.js');
 
-const create = async(req,res) => {
-    try{
-        const booking = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.ACCEPTED).json({
-            data : booking,
-            success : true,
-            message : "Booking created successfully",
-            err : {}
-        })
-    }catch(error){
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            data : {},
-            success : false,
-            message : "Something went wrong",
-            err : error
-        })
+class BookingController{
+    
+    async sendMessageToQueue(req,res){
+        try{
+            const channel = await createChannel();
+            const jsonmessage = JSON.stringify({message : "Success full"});
+            // console.log("channel created");
+            publishMessage(channel,REMINDER_BINDING_KEY,jsonmessage);
+            // console.log("message sent to queue");
+            return res.status(StatusCodes.ACCEPTED).json({
+                data : {},
+                success : true,
+                message : "Message sent to queue successfully",
+                err : {}
+            })
+        }
+        catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                data : {},
+                success : false,
+                message : "Something went wrong in sending message",
+                err : error
+            })
+        }
     }
-}
-
-const get = async(req,res) => {
-    try{
-        const booking = await bookingService.getBooking(req.params.id);
-        return res.status(StatusCodes.ACCEPTED).json({
-            data : booking,
-            success : true,
-            message : "Booking fetched successfully",
-            err : {}
-        })
-    }catch(error){
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            data : {},
-            success : false,
-            message : "Something went wrong",
-            err : error
-        })
+    async create(req,res){
+        try{
+            const booking = await bookingService.createBooking(req.body);
+            return res.status(StatusCodes.ACCEPTED).json({
+                data : booking,
+                success : true,
+                message : "Booking created successfully",
+                err : {}
+            })
+        }catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                data : {},
+                success : false,
+                message : "Something went wrong",
+                err : error
+            })
+        }
     }
-}
-
-const update = async(req,res) => {
-    try{
-        const booking = await bookingService.updateBooking(req.params.id,req.body);
-        return res.status(StatusCodes.ACCEPTED).json({
-            data : booking,
-            success : true,
-            message : "Booking updated successfully",
-            err : {}
-        })
-    }catch(error){
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            data : {},
-            success : false,
-            message : "Something went wrong",
-            err : error
-        })
+    async get(req,res){
+        try{
+            const booking = await bookingService.getBooking(req.params.id);
+            return res.status(StatusCodes.ACCEPTED).json({
+                data : booking,
+                success : true,
+                message : "Booking fetched successfully",
+                err : {}
+            })
+        }catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                data : {},
+                success : false,
+                message : "Something went wrong",
+                err : error
+            })
+        }
     }
-}
+    async update(req,res){
+        try{
+            const booking = await bookingService.updateBooking(req.params.id,req.body);
+            return res.status(StatusCodes.ACCEPTED).json({
+                data : booking,
+                success : true,
+                message : "Booking updated successfully",
+                err : {}
+            })
+        }catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                data : {},
+                success : false,
+                message : "Something went wrong",
+                err : error
+            })
+        }
+    }
+};
 
-module.exports = {create,get,update};
+
+
+
+module.exports = BookingController;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 //add routing logic
 
 const {BookingController} = require('./../../controllers/index');
+const bookingController = new BookingController();
 
-router.post('/bookings',BookingController.create);
-router.get('/bookings/:id',BookingController.get);
-router.patch('/bookings/:id',BookingController.update);
+router.post('/bookings',bookingController.create);
+router.post('/bookings/publish',bookingController.sendMessageToQueue);
+router.get('/bookings/:id',bookingController.get);
+router.patch('/bookings/:id',bookingController.update);
+
 
 module.exports  = router
