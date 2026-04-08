c52869da76159e4e27556bdd582041ced84a9081
SomeshIITH <shivjaiswal51230@gmail.com>
2026-04-08 15:34:17 +0530

    final touch done but not rut yet because it need payment service once making payment servie then run and check

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,9 +2,8 @@ const {StatusCodes} = require('http-status-codes');
 const {BookingService} = require('./../service/index.js');
 const bookingService = new BookingService();
 
-class BookingController{
     
-    async create(req,res,next){
+ const create =  async(req,res,next)=>{
         try{
             const booking = await bookingService.createBooking(req.body);
             return res.status(StatusCodes.CREATED).json({
@@ -17,35 +16,37 @@ class BookingController{
             next(error);
         }
     }
-    async get(req,res){
-        try{
-            const booking = await bookingService.getBooking(req.params.id);
-            return res.status(StatusCodes.ACCEPTED).json({
-                data : booking,
-                success : true,
-                message : "Booking fetched successfully",
-                err : {}
-            })
-        }catch(error){
-            next(error);
-        }
+   
+const get =  async (req,res,next) => {
+    try{
+        const booking = await bookingService.getBooking(req.params.id);
+        return res.status(StatusCodes.ACCEPTED).json({
+            data : booking,
+            success : true,
+            message : "Booking fetched successfully",
+            err : {}
+        })
+    }catch(error){
+        next(error);
     }
-    async update(req,res){
-        try{
-            const booking = await bookingService.updateBooking(req.params.id,req.body);
-            return res.status(StatusCodes.ACCEPTED).json({
-                data : booking,
-                success : true,
-                message : "Booking updated successfully",
-                err : {}
-            })
-        }catch(error){
-            next(error);
-        }
+}
+
+const update = async (req,res,next) => {
+    try{
+        const booking = await bookingService.updateBooking(req.params.id,req.body);
+        return res.status(StatusCodes.ACCEPTED).json({
+            data : booking,
+            success : true,
+            message : "Booking updated successfully",
+            err : {}
+        })
+    }catch(error){
+        next(error);
     }
+}
+
+
+
+module.exports = {
+    get,update,create
 };
-
-
-
-
-module.exports = BookingController;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const bookingSchema = require('./../../utils/booking-validate');
+const validate = require('./../../middlewares/validate-request');
 //add routing logic
 
 const {BookingController} = require('./../../controllers/index');
-const bookingController = new BookingController();
 
-router.post('/bookings',bookingController.create);
-router.post('/bookings/publish',bookingController.sendMessageToQueue);
-router.get('/bookings/:id',bookingController.get);
-router.patch('/bookings/:id',bookingController.update);
+
+router.post('/bookings',validate(bookingSchema),BookingController.create);
+router.get('/bookings/:id',BookingController.get);
+router.patch('/bookings/:id',BookingController.update);
 
 
 module.exports  = router
